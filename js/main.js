826e2fb930645c2b8653e6d324d2c590c3c9aef1
Mmy2000 <yahianada005@gmail.com>
2024-02-29 21:04:10 +0200

    starting js code

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var siteName = document.getElementById("websiteName");
+var siteUrl = document.getElementById("websiteUrl");
+sites = [];
+
+function addSite() {
+    var site = {
+        names : siteName.value,
+        url : siteUrl.value
+    }
+    sites.push(site);
+    console.log(sites);
+    
+}
